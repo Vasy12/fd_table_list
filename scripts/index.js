a623aca0d10bc4bf86933430703de86161ba8a77
Vasy Terkin <vasy.terkin12@gmail.com>
2020-05-13 16:11:44 +0300

    Add some code to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const userInput = prompt()
-alert(`your input: ${userInput}`)
 
 const sum = (...rest) => rest.reduce((total, item) => total + item, 0)
+(function () {
+  console.log("TEST");
+})()
