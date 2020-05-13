6188b575eeb97fe89a751b86c1138c8c65b7fab2
Vasy Terkin <vasy.terkin12@gmail.com>
2020-05-13 16:14:44 +0300

    Add fetch request to json

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
 'use strict'
 
-const userInput = prompt()
+const userInput = 'TEST'
 alert(`your input: ${userInput}`)
 
 const sum = (...rest) => rest.reduce((total, item) => total + item, 0)
+fetch('./users.json')
+  .then(response => response.json())
+  .then(console.log)
+  .catch(console.error)
